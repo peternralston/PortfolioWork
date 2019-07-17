927fa6ce7ea633ef261878773cdc3f2bd27111b3
peternralston <peternralston@gmail.com>
2019-07-16 21:09:19 -0500

    adding Factorizer

---
+++ Factorizor/Factorizor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetNumberFromUser();
+
+            Calculator.PrintFactors(number);
+            Calculator.IsPerfectNumber(number);
+            Calculator.IsPrimeNumber(number);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Prompt the user for an integer.  Make sure they enter a valid integer!
+        /// 
+        /// See the String Input lesson for TryParse() examples
+        /// </summary>
+        /// <returns>the user input as an integer</returns>
+        static int GetNumberFromUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Calculator
+    {
+        /// <summary>
+        /// Given a number, print the factors per the specification
+        /// </summary>
+        public static void PrintFactors(int number)
+        {
+
+        }
+
+        /// <summary>
+        /// Given a number, print if it is perfect or not
+        /// </summary>
+        public static void IsPerfectNumber(int number)
+        {
+
+        }
+
+        /// <summary>
+        /// Given a number, print if it is prime or not
+        /// </summary>
+        public static void IsPrimeNumber(int number)
+        {
+
+        }
+    }
+}
