ee4671c0a9fd4f7927cf24b21f0e4b282803b771
peternralston <peternralston@gmail.com>
2019-07-19 22:35:00 -0500

    added player object

---
+++ Battleship/BattleShip.UI/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BattleShip.UI
+{
+    internal class Player
+    {
+        private string _playerName;
+
+        public void SetPlayerName(string name)
+        {
+            _playerName = name;
+        }
+
+        public string GetPlayerName()
+        {
+            return _playerName;
+        }
+
+        internal void SetBoardPositions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
