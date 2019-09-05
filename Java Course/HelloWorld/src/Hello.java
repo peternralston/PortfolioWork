df9bbf5c6f38b1b1e6f8ec18fbba4e9909898dc4
peternralston <peternralston@gmail.com>
2019-09-05 17:12:26 -0500

    Covered Variables for java course

--- Java Course/HelloWorld/src/Hello.java
+++ Java Course/HelloWorld/src/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        
+        System.out.println("Hello Peter! Welcome to Java");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        System.out.println(myFirstNumber);
     };
 }
